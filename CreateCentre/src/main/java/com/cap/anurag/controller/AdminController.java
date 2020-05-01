8b20e1ff53ebd53138e91944fe614c2f6fa58651
MohanVamshiBoini <boinemohan@gmail.com>
2020-05-01 11:09:02 +0530

    Exception modified

--- CreateCentre/src/main/java/com/cap/anurag/controller/AdminController.java
+++ CreateCentre/src/main/java/com/cap/anurag/controller/AdminController.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cap.anurag.entities.DiagnosticCentre;
+import com.cap.anurag.exception.RecordFoundException;
 import com.cap.anurag.service.AdminService;
 
 @RestController
@@ -26,10 +26,10 @@ public class AdminController {
 	// Fetches Center Details and maps
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@PostMapping("/create")
-	public ResponseEntity<Boolean> create(@RequestBody DiagnosticCentre diagnosticCentre) {
+	public ResponseEntity<String> create(@RequestBody DiagnosticCentre diagnosticCentre) {
 		String centre = service.getCentre(diagnosticCentre.getCentreName());
 		if (centre != null) {
-			return new ResponseEntity(false, HttpStatus.FOUND);
+			throw new RecordFoundException("CentreName found");
 		} else {
 			Random rand = new Random();
 			int rand_int1 = rand.nextInt(1000);
@@ -39,9 +39,8 @@ public class AdminController {
 		}
 	}
 
-	@ResponseStatus(value = HttpStatus.FOUND, reason = "centre present")
-	@ExceptionHandler({ Exception.class })
-	public void handleException() {
-
+	@ExceptionHandler(RecordFoundException.class)
+	public ResponseEntity<String> userNotFound(RecordFoundException e) {
+		return new ResponseEntity<String>(e.getMessage(), HttpStatus.FOUND);
 	}
 }
